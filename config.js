45488541b0ae3ec855d2c61737d3be8368a6e99e
zngochoan <zngochoan@gmail.com>
2022-03-18 01:02:46 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Heluu Linh cute ',
+    introDesc: `Anh có điều muốn nói với em, phải trả lời thật lòng nhé `,
+    btnIntro: 'Okila',
+    title: 'Linh rất thích anh có phải khongg',
+    desc: 'không trả lời hoặc thoát ra là đồng ý ',
+    btnYes: 'Yepp',
+    btnNo: 'Không!',
+    question:'Nói cho anh biết Linh thích anh nhiều như thế nào đi',
+    btnReply: 'Accept',
+    reply: 'reply',
+    mess: 'Nhắn tin cho anh đi nàoo',
+    messDesc: '(*__*)',
+    btnAccept: 'Accept',
+    messLink: 'https://www.facebook.com/zngochoan/', //link fb
+    bgLink: 'https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/273711880_1964904957023048_468156812987526885_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9DH9d0i5j4sAX88BnxO&_nc_ht=scontent.fhan2-1.fna&oh=00_AT8nLRndVnrJSATXg8DKua1wwVWf_ln-TbggOFGKM8ig-w&oe=6237A161', //link ảnh nền
+    iputBgLink: 'https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/273711880_1964904957023048_468156812987526885_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9DH9d0i5j4sAX88BnxO&_nc_ht=scontent.fhan2-1.fna&oh=00_AT8nLRndVnrJSATXg8DKua1wwVWf_ln-TbggOFGKM8ig-w&oe=6237A161', //link iputBgLink
+    lookMeLink: 'https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/273711880_1964904957023048_468156812987526885_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9DH9d0i5j4sAX88BnxO&_nc_ht=scontent.fhan2-1.fna&oh=00_AT8nLRndVnrJSATXg8DKua1wwVWf_ln-TbggOFGKM8ig-w&oe=6237A161', //link ảnh lookMe
+    soundLink: 'https://files.fm/filebrowser#/Whistle-FloRida-6426947.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
