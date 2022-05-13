d30250bd1bb48b58fe965c4a15fe19fb3ec2e824
zngochoan <zngochoan@gmail.com>
2022-05-14 00:31:16 +0700

    zzz

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    introTitle: 'Heluu Linh cute ',
+    introTitle: 'Heluu Hà cute ',
     introDesc: `Anh có điều muốn nói với em, phải trả lời thật lòng nhé `,
     btnIntro: 'Okila',
-    title: 'Linh rất thích anh có phải khongg',
+    title: 'Hà rất thích anh có phải khongg',
     desc: 'không trả lời hoặc thoát ra là đồng ý ',
     btnYes: 'Yepp',
     btnNo: 'Không!',
@@ -13,9 +13,9 @@ const CONFIG = {
     messDesc: '(*__*)',
     btnAccept: 'Accept',
     messLink: 'https://www.facebook.com/zngochoan/', //link fb
-    bgLink: 'https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/273711880_1964904957023048_468156812987526885_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9DH9d0i5j4sAX88BnxO&_nc_ht=scontent.fhan2-1.fna&oh=00_AT8nLRndVnrJSATXg8DKua1wwVWf_ln-TbggOFGKM8ig-w&oe=6237A161', //link ảnh nền
+    bgLink: 'https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/83042038_948018468926539_8544394714458095616_n.jpg?_nc_cat=104&ccb=1-6&_nc_sid=174925&_nc_ohc=3Mmb_eLsdFMAX8XwAJz&_nc_ht=scontent.fhan14-1.fna&oh=00_AT93pDsPPAlinnVi3aofIHmYgpszI2nEC2MAn7BMCxrygA&oe=62A42D4B', //link ảnh nền
     iputBgLink: 'https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/273711880_1964904957023048_468156812987526885_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9DH9d0i5j4sAX88BnxO&_nc_ht=scontent.fhan2-1.fna&oh=00_AT8nLRndVnrJSATXg8DKua1wwVWf_ln-TbggOFGKM8ig-w&oe=6237A161', //link iputBgLink
-    lookMeLink: 'https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/273711880_1964904957023048_468156812987526885_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9DH9d0i5j4sAX88BnxO&_nc_ht=scontent.fhan2-1.fna&oh=00_AT8nLRndVnrJSATXg8DKua1wwVWf_ln-TbggOFGKM8ig-w&oe=6237A161', //link ảnh lookMe
+    lookMeLink: 'https://scontent.fhan14-1.fna.fbcdn.net/v/t1.6435-9/83042038_948018468926539_8544394714458095616_n.jpg?_nc_cat=104&ccb=1-6&_nc_sid=174925&_nc_ohc=3Mmb_eLsdFMAX8XwAJz&_nc_ht=scontent.fhan14-1.fna&oh=00_AT93pDsPPAlinnVi3aofIHmYgpszI2nEC2MAn7BMCxrygA&oe=62A42D4B', //link ảnh lookMe
     soundLink: 'https://files.fm/filebrowser#/Whistle-FloRida-6426947.mp3', //link nhac
     duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
     swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
